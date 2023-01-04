ec7678405872da7b50f409cbff3ba3cbb2d7401e
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2023-01-04 16:59:37 +0530

    json::)

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,25 @@
 
-const PORT = process.env.PORT || 3000;
-const path = require("path");
+// const PORT = process.env.PORT || 3000;
+// const path = require("path");
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, "db.json"));
+// const middlewares = jsonServer.defaults();
+// server.use(middlewares);
+// server.use(jsonServer.bodyParser);
+// server.use("/api", router);
+// server.listen(PORT, () =>
+//   console.log(`JSON Server is running on port ${PORT}`)
+// );
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3000;
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use("/api", router);
-server.listen(PORT, () =>
-  console.log(`JSON Server is running on port ${PORT}`)
-);
+server.use(router);
+server.listen(PORT);
+
+
+
